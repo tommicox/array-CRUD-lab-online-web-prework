78de0ac9a78cf9981f0349b30c9e78feec97e2e3
Tommi Cox <tommicox@gmail.com>
2019-03-03 21:30:36 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -14,7 +14,7 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  create_an_array.unshift ("Mandy")
+  create_an_array.unshift ("wow")
 
 end
 
